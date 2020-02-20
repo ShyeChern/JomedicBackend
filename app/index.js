a3f8ca93b895cd368fa2e0383b90adcaa0f9a95f
shay <zarann.sz@gmail.com>
2020-02-20 19:29:40 +0800

    no message

--- app/index.js
+++ app/index.js
@@ -1,11 +1,18 @@
 module.exports = (app) => {
   
-    // var frontLayerDefend = require('../app/controller/FLD');
-    // app.post('/query',frontLayerDefend.securityCheckPost);
     
     var adminC = require('../app/controller/JomMedic/Admin/adminController');
+    //app.post -- "post" can be change to "get" 
+  //                              adminfunction is function name created and exported in controller
+    app.post('/adminquery',adminC.adminfunction);
+
     var customerC = require('../app/controller/JomMedic/Customer/customerController');
+    app.post('customerquery',customerC.custfunction);
+
     var eWalletC = require('../app/controller/JomMedic/e_Wallet/ewalletController');
+    app.post('ewalletquery',eWalletC.ewallfunction);
+    
     var providerC = require('../app/controller/JomMedic/Provider/providerController');
+    app.post('providerquery',providerC.providerfunction);
     
   };
