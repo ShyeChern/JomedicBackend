dcf0fc56dc03991a15e450615f50275384ebdb06
shay <zarann.sz@gmail.com>
2020-02-20 18:31:59 +0800

    no message

--- config/connV2.js
+++ config/connV2.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql');
+require('dotenv').config();
 var pool  = mysql.createPool({
     connectionLimit : 100,
     host            : 'localhost',
-    user            : 'root',
-    password        : '#@!321Cba',
-    database        : 'emedica'
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASS,
+    database        : process.env.DB_NAME
   });
 
   module.exports = pool;
