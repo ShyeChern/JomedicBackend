43314278e848cb6b7ba7fce585ca85a6028380f9
Shye Chern Lim <46220892+ShyeChern@users.noreply.github.com>
2020-06-16 16:28:57 +0800

    Customer Backend

--- app/index.js
+++ app/index.js
@@ -1,18 +1,19 @@
 module.exports = (app) => {
-  
-    
+    var multer = require('multer');
+    var upload = multer();
+
     var adminC = require('../app/controller/JomMedic/Admin/adminController');
     //app.post -- "post" can be change to "get" 
-  //                              adminfunction is function name created and exported in controller
-    app.post('/adminquery',adminC.adminfunction);
+    //                              adminfunction is function name created and exported in controller
+    app.post('/adminquery', adminC.adminfunction);
 
     var customerC = require('../app/controller/JomMedic/Customer/customerController');
-    app.post('customerquery',customerC.custfunction);
+    app.post('/customerquery', upload.single('file'), customerC.custfunction);
 
     var eWalletC = require('../app/controller/JomMedic/e_Wallet/ewalletController');
-    app.post('ewalletquery',eWalletC.ewallfunction);
-    
+    app.post('/ewalletquery', eWalletC.ewallfunction);
+
     var providerC = require('../app/controller/JomMedic/Provider/providerController');
-    app.post('providerquery',providerC.providerfunction);
-    
-  };
+    app.post('/providerquery', providerC.providerfunction);
+
+};
--- config/connV2.js
+++ config/connV2.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 require('dotenv').config();
-var pool  = mysql.createPool({
-    connectionLimit : 100,
-    host            : 'localhost',
-    user            : process.env.DB_USER,
-    password        : process.env.DB_PASS,
-    database        : process.env.DB_NAME
-  });
+var pool = mysql.createPool({
+  connectionLimit: 100,
+  host: '157.245.148.221',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  multipleStatements: true
+});
 
-  module.exports = pool;
+module.exports = pool;
